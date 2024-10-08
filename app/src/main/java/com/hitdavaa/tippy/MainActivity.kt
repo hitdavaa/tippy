8c6768f9e5e9893d7b42042d150a2cbe9a15ff0b
hitdavaa <david123hun@gmail.com>
2024-10-08 16:56:38 +0200

    basic layout, seekbar feature added

--- app/src/main/java/com/hitdavaa/tippy/MainActivity.kt
+++ app/src/main/java/com/hitdavaa/tippy/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.hitdavaa.tippy
 
+import android.health.connect.datatypes.units.Percentage
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.temporal.TemporalAmount
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+    private lateinit var etBaseAmount: EditText
+    private lateinit var sbTip: SeekBar
+    private lateinit var tvTipPercentLabel: TextView
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        etBaseAmount = findViewById(R.id.etBaseAmount);
+        sbTip = findViewById(R.id.sbTip);
+        tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel);
+        tvTipAmount = findViewById(R.id.tvTipAmount);
+        tvTotalAmount = findViewById(R.id.tvTotalAmount);
+
+        sbTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                Log.i(TAG, "onProgressChanged $p1")
+                tvTipPercentLabel.text = "$p1%";
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
     }
 }
